6eda472f095f5275bb3ccd750508044f617e82cb
seven <qidehui@zhichubao.com>
2018-08-05 20:24:30 +0800

    提交一次

---
+++ sever.js
@@ -0,0 +1,50 @@
+var express = require('express'), //引入express模块
+
+    app = express(),
+
+    server = require('http').createServer(app);
+
+    io=require('socket.io').listen(server);
+    users=[];
+
+app.use('/', express.static(__dirname + '/www')); //指定静态HTML文件的位置
+
+server.listen(8081);
+
+
+io.on('connection',function(socket){
+    socket.on('login',function(nickname){
+        console.log(nickname);
+        if(users.indexOf(nickname)>-1){
+            socket.emit('nickExisted');
+        }else{
+            socket.userIndex=users.length;
+            socket.nickname=nickname;
+            users.push(nickname);
+              socket.emit('loginSuccess');
+             socket.broadcast.emit('system',nickname,users.length,'login');
+
+        }
+    });
+      //接收新消息
+      socket.on('postMsg', function(msg) {
+
+        //将消息发送到除自己外的所有用户
+
+        socket.broadcast.emit('newMsg', socket.nickname, msg);
+
+    });
+    // 发送图片
+    socket.on('img', function(imgData) {
+        //通过一个newImg事件分发到除自己外的每个用户
+         socket.broadcast.emit('newImg', socket.nickname, imgData);
+    
+    });
+    socket.on('disconncet',function(){
+        //将断开的人从用户中删除
+        users.splice(socket.userIndex,1);
+        // 通知所有用户
+        socket.broadcast.emit('system',socket.nickname,users.length,'logout');
+    })
+
+})
